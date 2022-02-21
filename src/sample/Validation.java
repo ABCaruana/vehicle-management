1336897e32369b7045802f3b2ad4d52c9d9ebf6f
Loaf <acaruanaop@gmail.com>
2022-02-21 17:42:18 +0000

    add validation

---
+++ src/sample/Validation.java
@@ -0,0 +1,48 @@
+package sample;
+
+import javafx.scene.control.Alert;
+
+import java.util.ArrayList;
+
+public class Validation {
+        public static boolean checkErrors(String brandChoice, String typeChoice,
+                                       String locationChoice, String modelChoice,
+                                       String colourChoice, String yearChoice) {
+            ArrayList<String> errorFields = new ArrayList<>();
+
+            if (brandChoice == null) {
+                errorFields.add("Vehicle Brand");
+            }
+            if (typeChoice == null) {
+                errorFields.add("Vehicle Type");
+            }
+            if (locationChoice == null) {
+                errorFields.add("Vehicle Location");
+            }
+            if (modelChoice.equals("")) {
+                errorFields.add("Vehicle Model");
+            }
+            if (colourChoice.equals("")) {
+                errorFields.add("Vehicle Colour");
+            }
+            try {
+                Integer.parseInt(yearChoice);
+            }
+            catch (NumberFormatException numberFormatException){
+                errorFields.add("Vehicle Model year");
+            }
+
+            if (errorFields.size() >= 1) {
+                // create a list of errors
+                StringBuilder finalErrorMessage = new StringBuilder("There are problems with some information in your vehicle. Please fix the following and try again:\n\n");
+                for (String error : errorFields) {
+                    finalErrorMessage.append("- ").append(error).append("\n");
+                }
+                Alert a = new Alert(Alert.AlertType.INFORMATION, finalErrorMessage.toString());
+                a.setTitle("Problems with vehicle");
+                a.showAndWait();
+                return false;
+            }
+            return true;
+        }
+}
