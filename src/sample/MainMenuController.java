66b5f962c0629f557be5bb10c309eb6c78fdb495
Loaf <acaruanaop@gmail.com>
2022-02-21 09:41:23 +0000

    add manu controller

---
+++ src/sample/MainMenuController.java
@@ -0,0 +1,61 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainMenuController {
+
+    @FXML
+    CheckBox newquayCheck;
+    @FXML
+    CheckBox plymouthCheck;
+    @FXML
+    CheckBox torquayCheck;
+    @FXML
+    CheckBox tavistockCheck;
+    @FXML
+    Button submitButton;
+
+    @FXML
+    private void submitForm(){
+        ArrayList<String> locations = getLocations();
+
+        // make sure at least one location is selected
+        if (locations.size() == 0) {
+            Alert noLocationsError = new Alert(Alert.AlertType.ERROR, "Please Choose at least one location.");
+            noLocationsError.setTitle("Error");
+            noLocationsError.showAndWait();
+        }
+
+        // get all vehicles from that location as a list of Vehicle Objects
+        ArrayList<Vehicle> vehicles = JSONProcessing.getVehicleByLocation(locations);
+        for (Vehicle vehicle : vehicles){
+            System.out.println(vehicle.getLocation());
+        }
+
+        // open TableView
+    }
+
+    private ArrayList<String> getLocations(){
+        CheckBox[] locations = new CheckBox[4];
+        locations[0] = newquayCheck;
+        locations[1] = plymouthCheck;
+        locations[2] = torquayCheck;
+        locations[3] = tavistockCheck;
+        ArrayList<String> output = new ArrayList<>();
+
+        for (CheckBox location : locations) {
+            if (location.isSelected()) {
+                output.add(location.getText());
+            }
+        }
+
+        return output;
+    }
+}
